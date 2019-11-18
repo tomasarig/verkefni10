21fbb805e5e77386bff6fcd63bdf7b55af1ebd58
tomasarig <37630852+tomasarig@users.noreply.github.com>
2019-11-18 23:12:47 +0000

    virkni favorites og útlit á myndum og vídjóum

--- src/lib/display-media.js
+++ src/lib/display-media.js
@@ -1,7 +1,8 @@
 import getRandomImage from "./nasa-api";
-import {save} from "./storage";
-
-
+import { save } from "./storage";
+import { createImgEl } from "./helpers";
+import { createVidEl } from "./helpers";
+import { load } from "./storage"
 
 // todo vísa í rétta hluti með import
 
@@ -12,6 +13,21 @@ let img; // mynd á forsíðu
 
 let image; // object sem inniheldur núverandi mynd á forsíðu.
 
+
+function displayImage(obj) {
+    if (obj.type === 'image') {
+        function goGo() {
+            createImgEl(obj);
+        }
+        let img = new Image();
+        img.src = obj.mediaUrl;
+        if (img.complete) { goGo(obj) }
+        img.addEventListener('load', goGo)
+    };
+    if (obj.type !== 'image') {
+        createVidEl(obj);
+    };
+}
 /*
  * Sækir nýja Mynd af handahófi frá Nasa API og birtir hana á forsíðunni
  * ásamt titli og texta.
@@ -19,52 +35,22 @@ let image; // object sem inniheldur núverandi mynd á forsíðu.
 function getNewImage() {
     let newImgPromise = getRandomImage();
     let disp = document.querySelector('.apod');
-    disp.querySelector('.apod__image').setAttribute('src', '')
     newImgPromise
-    .then((result) => {
-        if(!result.ok) {
-            console.log(result.json());
-            throw new Error('Non 200 status');
-        };
-        return result.json();
-    })
-    .then((data) => {
-        // type, mediaUrl, text, title
-        title = data.title;
-        text = data.explanation;
-        img = disp.querySelector('.apod__image')
-        image = data;
-        if (image.media_type !== 'image') {
-            console.log(image);
-            let vid = document.createElement('iframe');
-            let vidUrl = image.url + '?rel=0&autoplay=0&showinfo=0&autohide=1&border=0&wmode=opaque&enablejsapi=1&modestbranding=1&autohide=1&controls=0'
-            vid.setAttribute('class', 'apod__image');
-            //vid.setAttribute('height', 560);
-            //vid.setAttribute('width', 995);
-            vid.setAttribute('src', vidUrl);
-            img.replaceWith(vid);      
-        };
-        if (image.media_type === 'image') {
-            console.log('reynum aftur');
-            getNewImage();return;
-            let imgEl = document.createElement('img');
-            imgEl.setAttribute('src', image.url);
-            imgEl.setAttribute('class', 'apod__image');
-            img.replaceWith(imgEl)
-        };
-        disp.querySelector('.apod__title').textContent = title;
-        disp.querySelector('.apod__text').textContent = text;
-
-/**
- *      <img class="apod__image">
-      <h2 class="apod__title"></h2>
-      <p class="apod__text"></p>
- */
-
-    })
-    .catch((error) => {
-        console.error(error);
-    });
+        .then((result) => {
+            if (!result.ok) {
+                console.log(result.json());
+                throw new Error('Non 200 status');
+            };
+            return result.json();
+        })
+        .then((data) => {
+            // type, mediaUrl, text, title
+            image = { type: data.media_type, mediaUrl: data.url, text: data.explanation, title: data.title };
+            displayImage(image);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
     return;
 }
 
@@ -72,7 +58,7 @@ function getNewImage() {
  * Vistar núverandi mynd í storage.
  */
 function saveCurrentImage() {
-    save(image.media_type, image.url, image.explanation, image.title);
+    save(image.type, image.mediaUrl, image.text, image.title);
 }
 
 /*
@@ -85,10 +71,37 @@ export default function init(apod) {
     getNewImage();
 }
 
+
+function displayFavorite(img) {
+    let main = document.querySelector('.favorites-body')
+    if (img.type === 'image') {
+        let imgEl = document.createElement('img');
+        imgEl.setAttribute('src', img.mediaUrl);
+        imgEl.setAttribute('class', 'apod__image');
+        main.append(imgEl);
+        let title = document.createElement('h2');
+        title.setAttribute('class', 'apod__title');
+        title.innerText = img.title
+        main.append(title);
+    };
+    if (img.type !== 'image') {
+        let vid = document.createElement('iframe');
+        let vidUrl = img.mediaUrl + '?rel=0&autoplay=0&showinfo=0&autohide=1&border=0&wmode=opaque&enablejsapi=1&modestbranding=1&autohide=1&controls=0'
+        vid.setAttribute('class', 'apod__image apod__video');
+        vid.setAttribute('src', vidUrl);
+        main.append(vid);
+        let title = document.createElement('h2');
+        title.setAttribute('class', '.apod__title');
+        title.innerText = img.title;
+    }
+}
 /*
  * Fall fyrir favourites.html. Sér um að sækja allar vistuðu myndirnar og birta þær ásamt
  * titlum þeirra.
  */
 export function loadFavourites() {
-
+    let images = load();
+    if (images.length === 0) { return; }
+    images.forEach((img) => displayFavorite(img));
 }
+
--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -49,5 +49,45 @@ export function random_date() {
   //önnur leið
   //let days_interval = Math.floor((date2 - date1)/(1000*60*60*24));
   //date1.setDate(date1.getDate() + Math.floor(Math.random() * days_interval));
-  return randDate.toISOString().substring(0,10);
+  return randDate.toISOString().substring(0, 10);
+}
+
+
+
+/**
+ * skilar apod el með viðkomandi img eða vid
+ * @param {element} el element sem sett er efst í apod
+ */
+
+/**
+ * skilar iframe element með video
+ */
+export function createVidEl(info) {  
+  let apodEl = document.querySelector('.apod');
+  let vidEl = apodEl.querySelector('.apod__image apod__video')
+  let vid = document.createElement('iframe');
+  let vidUrl = info.mediaUrl + '?rel=0&autoplay=0&showinfo=0&autohide=1&border=0&wmode=opaque&enablejsapi=1&modestbranding=1&autohide=1&controls=0'
+  vid.setAttribute('class', 'apod__image');
+  vid.setAttribute('src', vidUrl);
+  apodEl.replaceChild(vid,vidEl);
+  let title = apodEl.querySelector('.apod__title');
+  title.innerText = info.title;
+  let text = apodEl.querySelector('.apod__text');
+  text.innerText = info.text;
+}
+
+/**
+ * skilar img element með mynd
+ */
+export function createImgEl(info){
+  let apodEl = document.querySelector('.apod');
+  let imgEL = apodEl.querySelector('.apod__image');
+  let img = document.createElement('img');
+  img.setAttribute('src', info.mediaUrl);
+  img.setAttribute('class', 'apod__image');
+  apodEl.replaceChild(img,imgEL);
+  let title = apodEl.querySelector('.apod__title');
+  title.innerText = info.title;
+  let text = apodEl.querySelector('.apod__text');
+  text.innerText = info.text;
 }
